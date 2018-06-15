43cc44704fcd39eeebc163d965b2ec310a7132b1
harshit verma <harshit8858@gmail.com>
2018-06-16 01:32:50 +0530

    google maps added

--- geo_app/views.py
+++ geo_app/views.py
@@ -3,10 +3,10 @@ import requests
 from django.views.decorators.debug import sensitive_variables
 
 
-
 @sensitive_variables('data')
 def home(request):
-    r = requests.get('http://freegeoip.net/json/')
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+    r = requests.get('http://freegeoip.net/json/%s' % ip_address)
     data = r.json()
 
-    return render(request, 'geo_app/geo_ip.html', {'ip':data['ip'], 'country':data['country_name']})
+    return render(request, 'geo_app/geo_ip.html', {'ip':data['ip'], 'country':data['country_name'], 'lat':data['latitude'], 'lon':data['longitude'], 'api':'AIzaSyBx6vrcFlszZb8kxjGhylF5nteLki4N8Nw' })
