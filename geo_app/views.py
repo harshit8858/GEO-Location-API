3cbef69a5e82f1fca8d951247ef56a5e5aa80f18
harshit verma <harshit8858@gmail.com>
2018-06-16 01:49:32 +0530

    cached information added

--- geo_app/views.py
+++ geo_app/views.py
@@ -5,10 +5,21 @@ from django.views.decorators.debug import sensitive_variables
 
 @sensitive_variables('data')
 def home(request):
-    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
-    r = requests.get('http://freegeoip.net/json/%s' % ip_address)
-    data = r.json()
+    is_cached = ('data' in request.session)
 
-    x = YOUR_API_KEY       #get your api key, go to developers.google.com/maps/web/ and click on “Get a Key”, generate an API for yourself.
+    if not is_cached:
+        ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
+        r = requests.get('http://freegeoip.net/json/%s' % ip_address)
+        # data = r.json()
+        request.session['data'] = r.json()
 
-    return render(request, 'geo_app/geo_ip.html', {'ip':data['ip'], 'country':data['country_name'], 'lat':data['latitude'], 'lon':data['longitude'], 'api':x })
+    data = request.session['data']
+    # x = YOUR_API_KEY      # get you apikey, go to developers.google.com/maps/web/ and click on “Get a Key”, generate an API for yourself.
+
+    return render(request, 'geo_app/geo_ip.html', {'ip':data['ip'],
+                                                   'country':data['country_name'],
+                                                   'lat':data['latitude'],
+                                                   'lon':data['longitude'],
+                                                   'api':'AIzaSyBx6vrcFlszZb8kxjGhylF5nteLki4N8Nw',
+                                                   'is_cached':is_cached })
+    # return render(request, 'geo_app/geo_ip.html', {'ip':data['ip'], 'country':data['country_name'], 'lat':data['latitude'], 'lon':data['longitude'], 'api':x })
